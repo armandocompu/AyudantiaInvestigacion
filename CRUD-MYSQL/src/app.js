a00328ecc94cd3d7e765e74d23ac730a0a396d15
mandi <mandito1209@gmail.com>
2022-10-08 00:05:59 -0700

    Servidor con Nodemon

--- CRUD-MYSQL/src/app.js
+++ CRUD-MYSQL/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
+// settings
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
